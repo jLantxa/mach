48ef1bfe7446332f7d95c1d173ef460f0d0cbfab
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-05-03 21:32:30 +0200

    Added template with static library

---
+++ template/bin/src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+#include "lib.h"
+
+int main(int argc, char* argv[]) {
+  printf("My function says %d\n", my_function());
+  puts("Hello, world!");
+  return 0;
+}
---
+++ template/lib/include/lib.h
@@ -0,0 +1,7 @@
+
+#ifndef LIB_H_
+#define LIB_H_
+
+int my_function();
+
+#endif  // LIB_H_
